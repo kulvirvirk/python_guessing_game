e33807bae561f11ab15d7c9fda89821d2ea228fd
kulvirvirk <kvirkdev@gmail.com>
2020-03-11 02:00:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# 1. get user input about their favorite food. 
+# 2. if user enter the name of your favorite food, say "Yep! so amazing!"
+# 3. if user enter some other, say "Yuck! that's not it!"
+# 4. say thanks at the end
