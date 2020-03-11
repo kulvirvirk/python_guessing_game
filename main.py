65dd9bcddfacb2bb4ebdb6dcf8c603739d892742
kulvirvirk <kvirkdev@gmail.com>
2020-03-11 02:02:06 +0000

    added the logic to match the favorite food

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 # 2. if user enter the name of your favorite food, say "Yep! so amazing!"
 # 3. if user enter some other, say "Yuck! that's not it!"
 # 4. say thanks at the end
+
+my_fav_food = 'chicken'
+user_fav_food = input("What's my favorite food: ")
+
+if my_fav_food == user_fav_food:
+  print('Yep! so amazing!')
+else:
+  print("Yuck! that's not it!")
+
+print('Thanks for playing the Guessing Game, Bye!')
